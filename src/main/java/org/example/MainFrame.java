9b45ac3069573678f9ffb5976d39401877981dd4
anderson-bassan <bassan.programador@gmail.com>
2022-08-28 16:13:48 -0300

    add card layout with grid layout

--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -5,15 +5,17 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     private final CardLayout cards;
+    private final JPanel parentPanel;
 
     public MainFrame() {
         this.cards = new CardLayout();
+        this.parentPanel = new JPanel();
 
         setSelf();
     }
 
     private void setSelf() {
-        setLayout(new GridLayout());
+        setLayout(cards);
         setMinimumSize(new Dimension(1080, 800));
         setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,7 +23,53 @@ public class MainFrame extends JFrame {
         pack();
     }
 
+    public void setAndAdd() {
+        var panel1 = new JPanel();
+        panel1.setBackground(Color.black);
+        parentPanel.add(panel1);
+
+        var panel2 = new JPanel();
+        panel2.setBackground(Color.GRAY);
+        parentPanel.add(panel2);
+
+        var panel3 = new JPanel();
+        panel3.setBackground(Color.darkGray);
+        parentPanel.add(panel3);
+
+        var panel4 = new JPanel();
+        panel4.setBackground(Color.magenta);
+        parentPanel.add(panel4);
+
+        var panel5 = new JPanel();
+        panel5.setBackground(Color.BLUE);
+        parentPanel.add(panel5);
+
+        var panel6 = new JPanel();
+        panel6.setBackground(Color.CYAN);
+        parentPanel.add(panel6);
+
+        var panel7 = new JPanel();
+        panel7.setBackground(Color.GREEN);
+        parentPanel.add(panel7);
+
+        var panel8 = new JPanel();
+        panel8.setBackground(Color.LIGHT_GRAY);
+        parentPanel.add(panel8);
+
+        var panel9 = new JPanel();
+        panel9.setBackground(Color.orange);
+        parentPanel.add(panel9);
+    }
+
+    private void setParent() {
+        parentPanel.setLayout(new GridLayout(3, 3));
+        add(parentPanel, "parent");
+        cards.show(this.getContentPane(), "parent");
+    }
+
     public void run() {
         setVisible(true);
+        setParent();
+        setAndAdd();
     }
 }
